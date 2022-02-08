7baaa434496959a1e0856fb9cbacca3fbe775e67
yoldashovmashrab <yoldashovmashrab6003@gmail.com>
2022-02-08 10:29:28 -0800

    ss

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
-let elForm = document.querySelector(".form");
-let elInput = document.querySelector(".input");
-let elList = document.querySelector(".list");
-let elPrevBtn = document.querySelector(".btn-1");
-let elNextBtn = document.querySelector(".btn-2");
-let elBtnList = document.querySelector(".button-list");
-let paginationBox = document.querySelector(".none");
-let filmName = "spider";
-let page = 1;
+window.addEventListener("DOMContentLoaded", () => {
 
-const renderElement = function(obj, element) {
-    setTimeout(() => {
-        elForm.style.display = "block"
-        paginationBox.style.display = "block"
-        img.style.display = "none"
+    let elForm = document.querySelector(".form");
+    let elInput = document.querySelector(".input");
+    let elList = document.querySelector(".list");
+    let elPrevBtn = document.querySelector(".btn-1");
+    let elNextBtn = document.querySelector(".btn-2");
+    let elBtnList = document.querySelector(".button-list");
+    let paginationBox = document.querySelector(".none");
+    let filmName = "spider";
+    let page = 1;
 
-        const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-        const newArray = obj.results;
+    const renderElement = function(obj, element) {
+        setTimeout(() => {
 
-        console.log(newArray);
+            elForm.style.display = "block"
+            paginationBox.style.display = "block"
+            img.style.display = "none"
 
-        newArray.forEach(film => {
+            const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+            const newArray = obj.results;
 
-            const html = `
+            console.log(newArray);
+
+            newArray.forEach(film => {
+
+                const html = `
         <li class="card" style="width: 18rem;">
         <img class="card-img-top" src="${IMG_PATH}${film.poster_path}" alt="Card image cap">
         <div class="card-body">
@@ -32,122 +35,126 @@ const renderElement = function(obj, element) {
         </li>
         `
 
-            element.insertAdjacentHTML('beforeend', html);
-        })
-    }, 2000);
-}
-
-const renderApi = async function(country) {
-
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=${page}&query="${country}`)
-
-    const data = await response.json();
-
-    elBtnList.innerHTML = null;
-
-    for (let i = 1; i <= data.total_pages; i++) {
-        let newLi = document.createElement("li")
-        let newButton = document.createElement("button")
-
-        newButton.textContent = i;
-
-        newButton.dataset.buttonId = newButton.textContent;
-
-        newButton.setAttribute("class", "btn btn-primary");
-
-        elBtnList.appendChild(newLi);
-        newLi.appendChild(newButton);
+                element.insertAdjacentHTML('beforeend', html);
+            })
+        }, 1500);
     }
 
+    const renderApi = async function(country) {
+        try {
+            const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=${page}&query="${country}`)
 
-    if (page == 1) {
-        elPrevBtn.disabled = true;
-    } else {
-        elPrevBtn.disabled = false;
+            const data = await response.json();
+
+            elBtnList.innerHTML = null;
+
+            for (let i = 1; i <= data.total_pages; i++) {
+                let newLi = document.createElement("li")
+                let newButton = document.createElement("button")
+
+                newButton.textContent = i;
+
+                newButton.dataset.buttonId = newButton.textContent;
+
+                newButton.setAttribute("class", "btn btn-primary");
+
+                elBtnList.appendChild(newLi);
+                newLi.appendChild(newButton);
+            }
+
+
+            if (page == 1) {
+                elPrevBtn.disabled = true;
+            } else {
+                elPrevBtn.disabled = false;
+            }
+
+            if (page == data.total_pages) {
+                elNextBtn.disabled = true;
+            } else {
+                elNextBtn.disabled = false;
+            }
+
+            renderElement(data, elList);
+            console.log(data);
+        } catch {
+            alert("kechirasiz bunday kino topilmadi")
+        }
     }
 
-    if (page == data.total_pages) {
-        elNextBtn.disabled = true;
-    } else {
-        elNextBtn.disabled = false;
-    }
+    elForm.addEventListener("submit", (evt) => {
 
-    renderElement(data, elList);
-    console.log(data);
-}
+        evt.preventDefault()
 
-elForm.addEventListener("submit", (evt) => {
+        elForm.style.display = "none"
+        paginationBox.style.display = "none"
+        img.style.display = "block"
 
-    evt.preventDefault()
-
-    elForm.style.display = "none"
-    paginationBox.style.display = "none"
-    img.style.display = "block"
-
-    let inputValue = elInput.value;
-    page = 1;
-
-    elList.innerHTML = null;
-
-    if (inputValue === "") {
+        let inputValue = elInput.value;
         page = 1;
-        renderApi(`${filmName}`);
-    }
 
-    renderApi(`${inputValue}`)
-})
+        elList.innerHTML = null;
+
+        if (inputValue === "") {
+            page = 1;
+            renderApi(`${filmName}`);
+        }
+
+        renderApi(`${inputValue}`)
+    })
 
 
-elNextBtn.addEventListener("click", () => {
-    setTimeout(() => {
+    elNextBtn.addEventListener("click", () => {
+        setTimeout(() => {
+            elForm.style.display = "none"
+            paginationBox.style.display = "none"
+            img.style.display = "block"
+
+            elList.innerHTML = null;
+            let inputValue = elInput.value;
+
+            page++
+
+            renderApi(`${inputValue || filmName}`);
+        }, 2000);
+    })
+
+    elPrevBtn.addEventListener("click", () => {
+        setTimeout(() => {
+            elForm.style.display = "none"
+            paginationBox.style.display = "none"
+            img.style.display = "block"
+
+            elList.innerHTML = null;
+
+            let inputValue = elInput.value;
+
+            page--
+            renderApi(`${inputValue || filmName}`)
+        }, 2000);
+    })
+
+    elBtnList.addEventListener("click", (evt) => {
+
         elForm.style.display = "none"
         paginationBox.style.display = "none"
         img.style.display = "block"
 
-        elList.innerHTML = null;
-        let inputValue = elInput.value;
+        if (evt.target.matches(".btn-primary")) {
 
-        page++
+            elList.innerHTML = null;
 
-        renderApi(`${inputValue || filmName}`);
-    }, 2000);
+            let inputValue = elInput.value;
+            let filmName = "spider";
+
+            let pageId = evt.target.dataset.buttonId;
+            page = pageId;
+
+            renderApi(`${inputValue || filmName}`)
+
+        }
+
+    })
+
+    renderApi(`${filmName}`)
 })
-
-elPrevBtn.addEventListener("click", () => {
-    setTimeout(() => {
-        elForm.style.display = "none"
-        paginationBox.style.display = "none"
-        img.style.display = "block"
-
-        elList.innerHTML = null;
-
-        let inputValue = elInput.value;
-
-        page--
-        renderApi(`${inputValue || filmName}`)
-    }, 2000);
-})
-
-elBtnList.addEventListener("click", (evt) => {
-
-    elForm.style.display = "none"
-    paginationBox.style.display = "none"
-    img.style.display = "block"
-
-    if (evt.target.matches(".btn-primary")) {
-
-        elList.innerHTML = null;
-
-        let inputValue = elInput.value;
-        let filmName = "spider";
-
-        let pageId = evt.target.dataset.buttonId;
-        page = pageId;
-
-        renderApi(`${inputValue || filmName}`)
-
-    }
-
-})
-
-renderApi(`${filmName}`)
